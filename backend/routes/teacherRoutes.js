1314ffcf90d4c75f7844c760acd7fb112643336a
mercadodp1904 <mercadodyanpaul@gmail.com>
2024-11-02 18:33:19 +0800

    added teacher route

---
+++ backend/routes/teacherRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { protect, authorizeRoles} from '../middleware/authMiddleware.js';
+const router = express.Router();
+
+import {
+    addGrade,
+    updateGrade,
+    updateProfile
+} from '../controllers/teacherController.js';
+
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+
+router
+    .route('/profile')
+    .put(protect, updateGrade)
+    .post(protect, addGrade)
+    .put(protect, updateProfile);
+
+export default router;
+
